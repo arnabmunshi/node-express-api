e723b26f58fe6e305e0786beb417dd1f14729295
arnab <arnab47stars@gmail.com>
2020-08-20 20:51:35 +0530

    add dotenv package

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 
 import usersRoutes from './routes/users.js';
 
+// express app
 const app = express();
-const PORT = 5000;
+
+// dotenv config
+dotenv.config();
 
 app.use(bodyParser.json());
 
@@ -12,4 +16,8 @@ app.get('/', (req, res) => res.send('Home Page'));
 
 app.use('/users', usersRoutes);
 
+// port config
+const PORT = process.env.PORT || 5000;
+
+// listen for connections
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
