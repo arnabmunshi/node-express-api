1516e599c4872edb4c489566228eac3fec97b361
arnab <arnab47stars@gmail.com>
2020-08-22 11:26:32 +0530

    Update README.md

--- index.js
+++ index.js
@@ -5,28 +5,26 @@ import bodyParser from 'body-parser';
 
 import usersRoutes from './routes/users.js';
 
+// dotenv config
+dotenv.config();
+
 // express app
 const app = express();
 
-// dotenv config
-dotenv.config();
+// port config
+const PORT = process.env.PORT || 5000;
 
 // database config
 mongoose.connect(process.env.DBURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then((result) => console.log('DB Connected'))
+.then((result) => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)))
 .catch((error) => console.log(error));
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Home Page'));
 
-app.use('/users', usersRoutes);
-
-// port config
-const PORT = process.env.PORT || 5000;
-
-// listen for connections
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+// routes middleware
+app.use('/api/users', usersRoutes);
